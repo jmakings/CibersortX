ef88f6a361d3c67b945b7b9dbe637985f90c09cb
Jeff Makings <jeffmakings@gmail.com>
2022-08-01 14:47:55 -0700

    Clean up CAVApsuedo.R

--- scripts/CAVApsuedo.R
+++ scripts/CAVApsuedo.R
@@ -4,35 +4,7 @@ library(ggplot2)
 library(patchwork)
 library("berryFunctions")
 
-# Jeff way of doing it (ofc did not work)
-# Load the PBMC dataset
-# pbmc.rna <- as.sparse(read.csv("/Users/jmakings/Desktop/CAVA_Data/raw.csv", sep=",", header = TRUE, row.names = 1))
-# pbmc.rna <- CollapseSpeciesExpressionMatrix(pbmc.rna)
-# pbmc.rna <- t(as.matrix(pbmc.rna))
-# 
-# metaDF <- data.frame(read.csv("/Users/jmakings/Desktop/CAVA_Data/final_metadata.csv", sep=",", header = TRUE, row.names = 1))
-
-
-#Seurat Object Setup 
-# pbmc <- CreateSeuratObject(counts=pbmc.rna, row.names=0)
-# 
-# rownames(pbmc@meta.data)
-# order(metaDF$Cell_Index, rownames(pbmc@meta.data))
-# rownames(pbmc@meta.data)
-# pbmc@meta.data<- cbind(metaDF, pbmc@meta.data)
-# pbmc@meta.data
-# x <- as.data.frame(pbmc@meta.data)
-# 
-# select_meta <- x %>% select(Cluster)
-# 
-# select_meta %>% group_by(Cluster) %>% summarize(n())
-# 
-# new_meta <- t(select_meta)
-# 
-# df_merged <- merge(df, select_meta, by=0)
-# df_merged %>% dplyr::group_by(Cluster) %>% summarize_all(funs(sum)) -> df_summed
-
-# restart using Sujit's method
+# Using Sujit's method
 
 # Load in data and create Seurat object 
 seurat <- as.sparse(read.csv("/Users/jmakings/Desktop/CAVA_Data/raw.csv", sep=",", header = TRUE, row.names = 1))
